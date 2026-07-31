e9e4f18c46126a7c2779819e8ddef1fb8f41ebe5
kuzpugonka <6599255@bk.ru>
2026-07-31 12:08:47 +0300

    add swiper

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const swiper = document.querySelector('.swiper').swiper;
 new WOW().init();
+
+const swiper = new Swiper(".swiper", {
+    speed: 400,
+  spaceBetween: 100,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+swiper.slideNext();
